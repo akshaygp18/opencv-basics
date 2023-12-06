ad7e87d037213e8d93a912555a4efdc0799cf6ba
akshaygp18 <akshaygp41@gmail.com>
2023-12-06 10:56:16 +0530

    blank image

--- src/contours.py
+++ src/contours.py
@@ -1,8 +1,11 @@
 import cv2 as cv
+import numpy as np
 
 img = cv.imread('imgs/beach.jpg')
-
 cv.imshow('Beach', img)
+
+blank = np.zeros(img.shape[:2], dtype='uint8')
+cv.imshow('Blank', blank)
 
 
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
@@ -19,6 +22,8 @@ cv.imshow('Threshold',thresh)
 
 contours, hierarchies = cv.findContours(thresh, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
 print(f'{len(contours)} contour(s) found!')
+
+
 
 
 
