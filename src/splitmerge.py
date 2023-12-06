1d8ed1845c1a21000a4f7d319ae66bcfa49724b6
akshaygp18 <akshaygp41@gmail.com>
2023-12-06 11:39:23 +0530

    merge the images

--- src/splitmerge.py
+++ src/splitmerge.py
@@ -4,6 +4,8 @@ import numpy as np
 img = cv.imread('imgs/beach.jpg')
 cv.imshow('Beach', img)
 
+# Split the image
+
 b, g, r = cv.split(img)
 cv.imshow("BLUE", b)
 cv.imshow("GREEN", g)
@@ -13,6 +15,12 @@ print(img.shape)
 print(b.shape)
 print(g.shape)
 print(r.shape)
+
+
+# Merge the images
+
+merged = cv.merge([b,g,r])
+cv.imshow('Merged Image', merged)
 
 cv.waitKey(0)
 cv.destroyAllWindows()
