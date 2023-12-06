ab99a7157db16fa095ac12a7b6eaa7aec3b41d55
akshaygp18 <akshaygp41@gmail.com>
2023-12-06 11:17:48 +0530

    BGR to RGB

--- src/color_spaces.py
+++ src/color_spaces.py
@@ -6,8 +6,8 @@ img = cv.imread('imgs/beach.jpg')
 cv.imshow('Beach', img)
 
 
-plt.imshow(img)
-plt.show()
+# plt.imshow(img)
+# plt.show()
 
 # BGR to grayscale image
 
@@ -22,9 +22,14 @@ cv.imshow('HSV', hsv)
 # BGR to LAB
 
 lab = cv.cvtColor(img, cv.COLOR_BGR2LAB)
-cv.imshow('Lab', lab)            
+cv.imshow('Lab', lab) 
+
+# BGR to RGB image
+
+rgb = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+cv.imshow('RGB', rgb)
 
 
 
-# cv.waitKey(0)
-# cv.destroyAllWindows()
+cv.waitKey(0)
+cv.destroyAllWindows()
