01b3416d451d00be1072f4fb0d721594001cb7c0
akshaygp18 <akshaygp41@gmail.com>
2023-12-06 15:50:44 +0530

    color histogram

--- src/histogram.py
+++ src/histogram.py
@@ -5,21 +5,41 @@ import matplotlib.pyplot as plt
 img = cv.imread('imgs/beach.jpg')
 cv.imshow('Beach', img)
 
-gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-cv.imshow('Gray', gray)
+blank = np.zeros(img.shape[:2], dtype='uint8')
+
+mask = cv.circle(blank, (img.shape[1]//2,img.shape[0]//2), 100, 255, -1)
+
+masked = cv.bitwise_and(img,img,mask=mask)
+cv.imshow('Mask', masked)
 
 # Grayscale Histogram
 
-gray_hist = cv.calcHist([gray], [0], None,[256],[0,256])
+# gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+# cv.imshow('Gray', gray)
+
+# gray_hist = cv.calcHist([gray], [0], None,[256],[0,256])
+
+# plt.figure()
+# plt.title('Grayscale Histogram')
+# plt.xlabel('Bins')
+# plt.ylabel('# of pixels')
+# plt.plot(gray_hist)
+# plt.xlim([0,256])
+# plt.show()
+
+# Color Histogram
 
 plt.figure()
-plt.title('Grayscale Histogram')
+plt.title('Colour Histogram')
 plt.xlabel('Bins')
 plt.ylabel('# of pixels')
-plt.plot(gray_hist)
-plt.xlim([0,256])
+colors = ('b', 'g', 'r')
+for i,col in enumerate(colors):
+    hist = cv.calcHist([img], [i], mask, [256], [0,256])
+    plt.plot(hist, color=col)
+    plt.xlim([0,256])
+
 plt.show()
 
-z
-# cv.waitKey(0)
+cv.waitKey(0)
 # cv.destroyAllWindows()
