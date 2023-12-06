6ee3f1eea0b63d308eb3e6c593d11d5e5f04332d
akshaygp18 <akshaygp41@gmail.com>
2023-12-06 11:11:38 +0530

    BGR to LAB

--- src/color_spaces.py
+++ src/color_spaces.py
@@ -14,5 +14,10 @@ cv.imshow('Grayscale', gray)
 hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
 cv.imshow('HSV', hsv)
 
+# BGR to LAB
+
+lab = cv.cvtColor(img, cv.COLOR_BGR2LAB)
+cv.imshow('Lab', lab)            
+
 cv.waitKey(0)
 cv.destroyAllWindows()
