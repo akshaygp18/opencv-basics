7d18e53dfa6569f50fe4fd50045dd5244e112114
akshaygp18 <akshaygp41@gmail.com>
2023-12-06 12:12:26 +0530

    bilateralFilter

--- src/smoothing.py
+++ src/smoothing.py
@@ -20,5 +20,10 @@ cv.imshow('Gaussian Blur', gauss)
 median = cv.medianBlur(img, 3)
 cv.imshow('Median', median)
 
+# Bilateral
+
+bilateral = cv.bilateralFilter(img, 10, 35, 25)
+cv.imshow('Bilateral', bilateral) 
+
 cv.waitKey(0)
 cv.destroyAllWindows()
