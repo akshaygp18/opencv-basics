6c3a1e710f81ab0f0929b0009f3442de533ce1d0
akshaygp18 <akshaygp41@gmail.com>
2023-12-06 16:47:35 +0530

    simple thresholding

---
+++ src/thresh.py
@@ -0,0 +1,19 @@
+import cv2 as cv
+import numpy as np
+
+img = cv.imread('imgs/beach.jpg')
+cv.imshow('Beach', img)
+
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+cv.imshow('Gray', gray)
+
+# Simple thresholding
+
+threshold, thresh = cv.threshold(gray, 150, 255, cv.THRESH_BINARY)
+cv.imshow('Threshold', thresh)
+
+threshold, thresh_inv = cv.threshold(gray, 150, 255, cv.THRESH_BINARY_INV)
+cv.imshow('Simple Threshold Inv', thresh_inv)
+
+cv.waitKey(0)
+cv.destroyAllWindows()
