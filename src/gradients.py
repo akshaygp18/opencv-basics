95d798098c57d65b78a1e8fa833cfca037627cdb
akshaygp18 <akshaygp41@gmail.com>
2023-12-07 11:19:29 +0530

    sobel func

--- src/gradients.py
+++ src/gradients.py
@@ -14,8 +14,16 @@ lap = cv.Laplacian(gray, cv.CV_64F)
 lap = np.uint8(np.absolute(lap))
 cv.imshow('Laplacian', lap)
 
+# Sobel
+
+sobelx = cv.Sobel(gray, cv.CV_64F, 1, 0)
+sobely = cv.Sobel(gray, cv.CV_64F, 0, 1)
+combined_sobel = cv.bitwise_or(sobelx, sobely)
 
 
+cv.imshow('Sobelx', sobelx)
+cv.imshow('Sobely', sobely)
+cv.imshow('Combined Sobel', combined_sobel)
 
 
 cv.waitKey(0)
