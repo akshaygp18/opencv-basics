a39e431126b93d4c5be8f735e823e0260934b7a6
akshaygp18 <akshaygp41@gmail.com>
2023-12-07 10:47:42 +0530

    adaptive thresholding

--- src/thresh.py
+++ src/thresh.py
@@ -15,5 +15,11 @@ cv.imshow('Threshold', thresh)
 threshold, thresh_inv = cv.threshold(gray, 150, 255, cv.THRESH_BINARY_INV)
 cv.imshow('Simple Threshold Inv', thresh_inv)
 
+
+# Adaptive thresholding 
+
+adaptive_threshold = cv.adaptiveThreshold(gray, 255, cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY, 11,3)
+cv.imshow('Adaptive Threshold', adaptive_threshold)
+
 cv.waitKey(0)
 cv.destroyAllWindows()
